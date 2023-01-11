4a694f630167701ac34058577d39bd69c36f6e37
Tz <99411749+TzRico@users.noreply.github.com>
2023-01-11 17:10:21 -0300

    Add files via upload

---
+++ [1] Instalador.bat
@@ -0,0 +1,11 @@
+@echo off
+title Módulos de inicialização v.2.1.0
+:top
+cls
+pause
+    call npm install discord.js-self
+pause
+cls
+pause
+exit
+goto :top
---
+++ [2] Ligar.bat
@@ -0,0 +1,7 @@
+@echo off
+title SelfCall by Tz
+:top
+cls
+node .
+pause
+goto :top
